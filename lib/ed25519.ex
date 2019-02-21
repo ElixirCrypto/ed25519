45438972452ed77814e96c9ec072a2126c2ed6e9
Sun <sun@blockheaders.com>
2019-02-21 17:21:25 +0800

    init project

---
+++ lib/ed25519.ex
@@ -0,0 +1,18 @@
+defmodule Ed25519 do
+  @moduledoc """
+  Documentation for Ed25519.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ed25519.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
